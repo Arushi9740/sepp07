131901f723cfd634320515c246cf5d3c2fd07d94
Arushi9740 <arushimuskan004@gmail.com>
2025-04-29 19:25:25 +0530

    adding file

--- test03.py
+++ test03.py
@@ -3,8 +3,8 @@ import sepp03 as s3
 def add_beneficiary_valid():
     assert s3.add_beneficiary("abc","123456789") == "beneficiary added"
 
-def view_beneficiary_invalid():
-    assert s3.view_beneficiary("def") == "not found"
+# def view_beneficiary_invalid():
+#     assert s3.view_beneficiary("def") == "not found"
 
-def fund_transfer_success():
-    assert s3.fund_transfer("abc",30000) == "Transfer successful"
+# def fund_transfer_success():
+#     assert s3.fund_transfer("abc",30000) == "Transfer successful"
